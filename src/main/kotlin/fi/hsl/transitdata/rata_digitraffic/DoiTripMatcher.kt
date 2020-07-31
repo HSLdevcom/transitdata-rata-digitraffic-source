70b1c25312ad6c40123fbbce6d2bb148ca6dbff2
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-07-31 15:05:34 +0300

    Create helper for matching trains to trips

---
+++ src/main/kotlin/fi/hsl/transitdata/rata_digitraffic/DoiTripMatcher.kt
@@ -0,0 +1,46 @@
+package fi.hsl.transitdata.rata_digitraffic
+
+import fi.hsl.transitdata.rata_digitraffic.model.digitraffic.TimetableRow
+import fi.hsl.transitdata.rata_digitraffic.model.digitraffic.Train
+import fi.hsl.transitdata.rata_digitraffic.model.doi.TripInfo
+import fi.hsl.transitdata.rata_digitraffic.utils.LoggerDelegate
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+class DoiTripMatcher(private val zoneId: ZoneId, private val doiTrips: Collection<TripInfo>, private val doiStopMatcher: DoiStopMatcher) {
+    companion object {
+        private val log by LoggerDelegate()
+    }
+
+    fun matchTrainToTrip(train: Train): TripInfo? {
+        val possibleTrips = doiTrips.filter { trip ->
+            fun matches(timetableRow: TimetableRow, stopNumber: String, scheduledTime: LocalDateTime): Boolean {
+                return doiStopMatcher.checkIfStationContainsStop(timetableRow.stationShortCode, stopNumber)
+                        && timetableRow.scheduledTime.withZoneSameInstant(zoneId).toLocalDateTime() == scheduledTime
+            }
+
+            val firstTimetableRow = train.timeTableRows.first()
+            val lastTimetableRow = train.timeTableRows.last()
+
+            val sameFirstOrLastTimetableRow = matches(firstTimetableRow, trip.startStopNumber, trip.startTimeAsLocalDateTime) || matches(lastTimetableRow, trip.endStopNumber, trip.endTimeAsLocalDateTime)
+
+            //Check that the train has same commuter line ID (A, P, K, etc.) and either the first or last timetable row has same station and scheduled time
+            return@filter train.commuterLineID == trip.commuterLineID && sameFirstOrLastTimetableRow
+        }
+
+        if (possibleTrips.isEmpty()) {
+            log.warn("No trips found for train {} ({}) departing at {} from {}",
+                    train.trainNumber,
+                    train.commuterLineID ?: "",
+                    train.timeTableRows.first().scheduledTime,
+                    train.timeTableRows.first().stationShortCode)
+            return null
+        }
+
+        if (possibleTrips.size > 1) {
+            log.warn("More than 1 possible trip found for train {}", train.trainNumber)
+        }
+
+        return possibleTrips.random()
+    }
+}
