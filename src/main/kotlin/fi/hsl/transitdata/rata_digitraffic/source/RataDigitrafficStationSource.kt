4c84588555be4d5cdd792aca39e616d6cf7cb024
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-08-18 14:39:06 +0300

    Add identification headers when doing requests to rata.digitraffic.fi API

--- src/main/kotlin/fi/hsl/transitdata/rata_digitraffic/source/RataDigitrafficStationSource.kt
+++ src/main/kotlin/fi/hsl/transitdata/rata_digitraffic/source/RataDigitrafficStationSource.kt
@@ -2,6 +2,7 @@ package fi.hsl.transitdata.rata_digitraffic.source
 
 import fi.hsl.transitdata.rata_digitraffic.model.digitraffic.Station
 import fi.hsl.transitdata.rata_digitraffic.utils.JsonHelper
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.apache.pulsar.shade.org.apache.http.HttpStatus
@@ -10,10 +11,19 @@ import java.io.IOException
 class RataDigitrafficStationSource(private val httpClient: OkHttpClient, private val endpoint: String = "https://rata.digitraffic.fi/api/v1") {
     companion object {
         private const val METADATA_STATIONS = "/metadata/stations"
+
+        //https://www.digitraffic.fi/ohjeita/#sovelluksen-yksil%C3%B6iv%C3%A4t-otsikkotiedot
+        private val IDENTIFICATION_HEADERS = Headers.headersOf(
+            "Digitraffic-User", "HSL",
+            "User-Agent", "transitdata-rata-digitraffic-source/${RataDigitrafficStationSource::class.java.`package`.implementationVersion ?: "dev"}"
+        )
     }
 
     fun getStations(): List<Station> {
-        val request = Request.Builder().url(endpoint + METADATA_STATIONS).build()
+        val request = Request.Builder()
+                .url(endpoint + METADATA_STATIONS)
+                .headers(IDENTIFICATION_HEADERS)
+                .build()
         val response = httpClient.newCall(request).execute()
 
         if (response.code == HttpStatus.SC_OK) {
