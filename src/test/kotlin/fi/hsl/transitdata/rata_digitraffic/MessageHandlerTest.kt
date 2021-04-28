5ea289ba1e249646d619e0478929ef50826fbaf4
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2021-04-28 14:14:25 +0300

    Update tests

--- src/test/kotlin/fi/hsl/transitdata/rata_digitraffic/MessageHandlerTest.kt
+++ src/test/kotlin/fi/hsl/transitdata/rata_digitraffic/MessageHandlerTest.kt
@@ -1,7 +1,6 @@
 package fi.hsl.transitdata.rata_digitraffic
 
 import com.google.protobuf.ByteString
-import com.google.protobuf.CodedInputStream
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.argThat
 import com.nhaarman.mockitokotlin2.doReturn
@@ -9,16 +8,16 @@ import com.nhaarman.mockitokotlin2.mock
 import fi.hsl.common.mqtt.proto.Mqtt
 import fi.hsl.common.pulsar.PulsarApplicationContext
 import fi.hsl.common.transitdata.proto.InternalMessages
-import fi.hsl.transitdata.rata_digitraffic.model.digitraffic.Train
+import fi.hsl.transitdata.rata_digitraffic.model.digitraffic.Station
+import fi.hsl.transitdata.rata_digitraffic.model.doi.JourneyPatternStop
 import fi.hsl.transitdata.rata_digitraffic.model.doi.StopPoint
 import fi.hsl.transitdata.rata_digitraffic.model.doi.TripInfo
 import org.apache.pulsar.client.api.*
-import org.apache.pulsar.shade.io.netty.handler.codec.protobuf.ProtobufDecoder
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
 import org.mockito.Mockito
 import java.net.URL
+import java.time.ZoneId
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.function.BiFunction
@@ -26,21 +25,26 @@ import java.util.function.BiFunction
 
 
 class MessageHandlerTest {
+    //Used for test data
+    private val stationCodes = listOf("KKN", "TOL", "JRS", "MAS", "KLH", "EPO", "TRL", "KVH", "KNI", "KEA", "KIL", "LPV", "HPL", "PSL", "HKI")
+    
+    //Helper functions for generating mock data
+    private fun generateStopPoints(count: Int): List<StopPoint> = (1..count).map { index -> StopPoint((1000 + index).toString(), "1", index.toDouble(), index.toDouble()) }
 
+    private fun generateStations(count: Int): List<Station> = (1..count).map { index -> Station(true, stationCodes[(index - 1) % stationCodes.size], index.toDouble(), index.toDouble()) }
+
+    private fun generateJourneyPatternStops(count: Int): List<JourneyPatternStop> = (1..count).map { index -> JourneyPatternStop("1", (1000 + index).toString(), index) }
 
     lateinit var mockContext: PulsarApplicationContext
     lateinit var mockConsumer: Consumer<ByteArray>
     lateinit var mockTripUpdateProducer : Producer<ByteArray>
     lateinit var mockTrainCancellationProducer : Producer<ByteArray>
     lateinit var messageHandler : MessageHandler
-    lateinit var doiStopMatcher: DoiStopMatcher
-    lateinit var doiTripMatcher: DoiTripMatcher
     lateinit var tripUpdateMockTypeMessageBuilder : TypedMessageBuilder<ByteArray>
     lateinit var trainCancellationMockTypeMessageBuilder : TypedMessageBuilder<ByteArray>
 
     @Before
-    fun before(){
-
+    fun before() {
         tripUpdateMockTypeMessageBuilder = mock<TypedMessageBuilder<ByteArray>>{
             on{eventTime(any<Long>())} doReturn (it)
             on{property(any<String>(), any<String>())} doReturn(it)
@@ -64,18 +68,19 @@ class MessageHandlerTest {
         mockTrainCancellationProducer = mock<Producer<ByteArray>>{
             on{newMessage()} doReturn (trainCancellationMockTypeMessageBuilder)
         }
-        doiStopMatcher = mock<DoiStopMatcher>{
-            on{getStopPointForStationAndTrack(any<String>(), any<Int>())} doReturn StopPoint("1000", "dummy stop", 12.34, 56.78)
-        }
-        doiTripMatcher = mock<DoiTripMatcher>{
-            on{matchTrainToTrip(any<Train>())} doReturn (TripInfo("dvjId", "routeId", "20201203", "00:01:02", "01:01:02", 1, "1000", "2000", "1234"))
-        }
         mockContext = mock<PulsarApplicationContext>{
             on{consumer} doReturn (mockConsumer)
             on{producers} doReturn (mapOf("feedmessage-tripupdate" to mockTripUpdateProducer, "feedmessage-train-cancelled" to mockTrainCancellationProducer))
         }
 
-        messageHandler = MessageHandler(mockContext, doiStopMatcher, doiTripMatcher)
+        val trips = listOf(TripInfo("1", "3001U", "20200822", "12:37:00", "13:18:00", 2, "1001", "1014", "U", "1"))
+        val stops = generateStopPoints(15)
+        val stations = generateStations(15)
+        val journeyPatternStops = mapOf("1" to generateJourneyPatternStops(15))
+
+        messageHandler = MessageHandler(mockContext, false, ZoneId.of("Europe/Helsinki"), Metadata(
+            trips, stops, journeyPatternStops, stations
+        ))
     }
 
     private fun createMessage(path : String) : Message<Any>{
@@ -114,7 +119,7 @@ class MessageHandlerTest {
         //Build message
         val message = createMessage("confirmed_train.json")
         messageHandler.handleMessage(message)
-        Mockito.verify(mockConsumer, Mockito.times(2)).acknowledgeAsync(any<MessageId>())
+        Mockito.verify(mockConsumer, Mockito.times(1)).acknowledgeAsync(any<MessageId>()) //Message should be acknowledged
         Mockito.verify(mockTripUpdateProducer, Mockito.times(1)).newMessage()
         Mockito.verify(trainCancellationMockTypeMessageBuilder).value(argThat{
             val tripCancellation = InternalMessages.TripCancellation.parseFrom(this)
@@ -129,7 +134,7 @@ class MessageHandlerTest {
     fun handleCancellationMessageTest(){
         val message = createMessage("cancelled_train.json")
         messageHandler.handleMessage(message)
-        Mockito.verify(mockConsumer, Mockito.times(2)).acknowledgeAsync(any<MessageId>())
+        Mockito.verify(mockConsumer, Mockito.times(1)).acknowledgeAsync(any<MessageId>()) //Message should be acknowledged
         Mockito.verify(mockTripUpdateProducer, Mockito.times(1)).newMessage()
         Mockito.verify(trainCancellationMockTypeMessageBuilder).value(argThat{
             val tripCancellation = InternalMessages.TripCancellation.parseFrom(this)
